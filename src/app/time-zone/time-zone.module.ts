2acd20e7f6d9104173c8ebd401c2f3a04f1c66c7
Connie Leung <cleung334@gmail.com>
2018-05-22 12:15:21 +0800

    Refactor etc dropdown to component

--- src/app/time-zone/time-zone.module.ts
+++ src/app/time-zone/time-zone.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { InputTimeFormComponent } from "./input-time-form/input-time-form.component";
+import { UtcDropdownComponent } from "./utc-dropdown/utc-dropdown.component";
 
 @NgModule({
   imports: [CommonModule, HttpClientModule, FormsModule, NgbModule],
-  declarations: [InputTimeFormComponent],
+  declarations: [InputTimeFormComponent, UtcDropdownComponent],
   entryComponents: [InputTimeFormComponent],
-  exports: [InputTimeFormComponent]
+  exports: [InputTimeFormComponent, UtcDropdownComponent]
 })
 export class TimeZoneModule {}
---
+++ src/app/time-zone/utc-dropdown/utc-dropdown.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+
+@Component({
+  selector: "utc-dropdown",
+  templateUrl: "./utc-dropdown.component.html",
+  styleUrls: ["./utc-dropdown.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UtcDropdownComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
