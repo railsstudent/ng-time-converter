82e436960aaaa0365405e75cf6d996a43c32902f
Connie Leung <cleung334@gmail.com>
2018-05-26 14:01:28 +0800

    Add offset table

--- src/app/time-zone/utc-dropdown/utc-dropdown.component.ts
+++ src/app/time-zone/utc-dropdown/utc-dropdown.component.ts
@@ -6,22 +6,45 @@ import {
   Output,
   EventEmitter
 } from "@angular/core";
-import { map, merge } from "lodash-es";
+import { map, merge, uniqBy } from "lodash-es";
 
 import { UtcInfo, TimeInfo } from "../shared/index";
 
 @Component({
   selector: "utc-dropdown",
   templateUrl: "./utc-dropdown.component.html",
-  styleUrls: [],
+  styleUrls: ["./utc-dropdown.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UtcDropdownComponent implements OnInit {
-  @Input() labelName: string;
-  @Input() timeZones: UtcInfo[];
-  @Input() timeZone: UtcInfo;
 
+  @Input() labelName: string;
   @Output() selectedTimeZone = new EventEmitter<UtcInfo>();
+
+  offsets: number[];
+  allTimezones: UtcInfo[];
+  selectedOffset: UtcInfo;
+
+  @Input()
+  get timeZone(): UtcInfo {
+    return this.selectedOffset;
+  }
+
+  set timeZone(value: UtcInfo) {
+    this.selectedOffset = value;
+  }
+
+  @Input()
+  get timeZones(): UtcInfo[] {
+    return this.allTimezones;
+  }
+
+  set timeZones(values: UtcInfo[]) {
+    this.allTimezones = values;
+    if (values) {
+      this.offsets = map(uniqBy(values, tz => tz.offset), tz => tz.offset);
+    }
+  }
 
   constructor() {}
 
@@ -31,8 +54,22 @@ export class UtcDropdownComponent implements OnInit {
     console.log("UtcDropdownComponent timeZone", this.timeZone);
   }
 
+  setStyles(offset) {
+    const sameOffset = this.selectedOffset && this.selectedOffset.offset === offset;
+    const styles = {
+      color: sameOffset ? 'white': 'black',
+    }
+    if (sameOffset) {
+      styles['background-color'] = 'rebeccapurple';
+    } /*else {
+      styles['background'] = 'none';
+    }*/
+    return styles;
+  }
+
   timeZoneChanged($event) {
     console.log("timeZoneChanged", $event);
+    this.selectedOffset = $event;
     this.selectedTimeZone.emit($event);
   }
 }
