b27e360b1a31015a409e893dd802fea488ff372b
Connie Leung <cleung334@gmail.com>
2018-06-05 23:25:18 +0800

    Set active state to selected button

--- src/app/time-zone/input-theme/input-theme.component.ts
+++ src/app/time-zone/input-theme/input-theme.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit, Output, Input, EventEmitter } from "@angular/core";
+import {
+  Component,
+  Output,
+  Input,
+  EventEmitter,
+  ViewEncapsulation
+} from "@angular/core";
 
 @Component({
   selector: "time-theme",
   templateUrl: "./input-theme.component.html",
-  styleUrls: ["./input-theme.component.scss"]
+  styleUrls: ["./input-theme.component.scss"],
+  encapsulation: ViewEncapsulation.Native
 })
-export class InputThemeComponent implements OnInit {
+export class InputThemeComponent {
   themes = ["blue", "green", "rebeccapurple", "red", "firebrick", "goldenrod"];
 
   @Input() theme: string;
   @Output()
   public themeChange: EventEmitter<string> = new EventEmitter<string>();
-  constructor() {}
-
-  ngOnInit() {}
 
   setTheme(color) {
     console.log("setTheme", color);
