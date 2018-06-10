c038d8a8c544f23c80842673d04ad2adf65fb70d
Connie Leung <cleung334@gmail.com>
2018-06-10 11:25:56 +0800

    Separate into required error and invalid error

--- src/app/time-zone/validators/utc.validator.ts
+++ src/app/time-zone/validators/utc.validator.ts
@@ -11,19 +11,25 @@ import { get } from "lodash-es";
 function utcValidator(): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } => {
     const timeZone = control.value;
-    const err = {
+    const invalidErr = {
       timezone: {
         invalid: true
       }
     };
 
-    if (!timeZone) {
-      return err;
+    const requiredErr = {
+      timezone: {
+        required: true
+      }
+    };
+
+    if (!timeZone || (typeof timeZone === "string" && timeZone === "")) {
+      return requiredErr;
     }
     const description = get(timeZone, "description", null);
     const offset = get(timeZone, "offset", null);
     const utc = get(timeZone, "utc", null);
-    return description == null || offset == null || utc == null ? err : null;
+    return description == null || offset == null || utc == null ? invalidErr : null;
   };
 }
 
